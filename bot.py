73adf60e57f6d177efba92749cf652a0f8f2db08
root <root@vds23624.vpsza500.kz>
2025-07-31 11:22:40 +0500

    zz

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # API-токен бота из переменной окружения или значения по умолчанию
-TOKEN = os.environ.get('TOKEN', '7991006616:AAEbylMKn7nGDDQeY_fMmkNQ-y8RvvhZCFI')
+TOKEN = os.environ.get('TOKEN', '7339317818:AAEeTxHPrd0BsqSlMFk41ZFMdvp33t8DyjU')
 
 # Telegram ID администратора
 ADMIN_IDS = ['1645154232'] # Должен быть список, даже с одним ID
